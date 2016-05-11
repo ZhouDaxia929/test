21483b6fd9950a65f28c98a7ceaa6cf436527061
周大侠 <zhoudongxu929@outlook.com>
2016-05-11 15:47:43 +0800

    树相关

--- test/1.cpp
+++ test/1.cpp
@@ -1,56 +1,252 @@
-#include <iostream>
-#include <cstdio>
-using namespace std;
+#include <stdio.h>
+#include <stdlib.h>
+#include <malloc.h>
+#include <string.h>
+#define m 3
+#define MAXLEN 100
+typedef char datatype;
+/*
+typedef struct node {
+	datatype data;
+	struct node *child[m];
+}node;
+typedef node *tree;
 
-//最长上升子序列(n^2)模板
-//入口参数：1.数组名称 2.数组长度（注意从1号位置开始）
-//状态转移方程：dp(i)=max{1,dp[j]+1}  (j=1,2,3,...,i-1 且 A[j]<A[i])
-template<class T>
-int LIS(T a[], int n)
-{
-	int i, j;
-	int ans = 0;
-	int m = 0;
-	int *dp = new int[n + 1];
-	
-	/*
-	//这个方法似乎比较麻烦，且不太符合状态转换方程
-	dp[1] = 1;
-	for (i = 2;i <= n;i++){  //确定当前起点
-		m = 0;   //以dp[i]为根的正确序列长度
-		for (j = 1;j<i;j++){
-			if (dp[j]>m&&a[j]<a[i])
-				m = dp[j];   //满足条件，更新结果
-		}
-		dp[i] = m + 1;
-		if (dp[i]>ans)
-			ans = dp[i];
+tree createtree() {
+	int i;
+	char ch;
+	tree t;
+	if ((ch = getchar()) == '#')
+		t = NULL;
+	else {
+		t = (tree)malloc(sizeof(node));
+		t->data = ch;
+		for (i = 0;i < m;++i)
+			t->child[i] = createtree();
 	}
-	*/
-	//标准程序
-	for (i = 1; i <= n; i++){
-		dp[i] = 1;
-		for (j = 1; j <= i - 1; j++){
-			if (a[j] < a[i] && dp[i] < dp[j] + 1) {   //这里应用状态转移方程
-				dp[i] = dp[j] + 1;
-			}
-		}
-		/* 记录最长子序列 */
-		if (dp[i] > ans) ans = dp[i];
+	return t;
+}
+*/
+
+/*
+//双亲表示法
+#define MAXSIZE 100
+typedef struct node {
+	datatype data;
+	int parent;
+}node;
+typedef struct tree {
+	node treelist[MAXSIZE];
+	int length, root;
+}tree;
+
+
+
+typedef struct node {
+	datatype data;
+	int parent;
+}node;
+
+typedef struct tree {
+	node treelist[MAXLEN];
+	int length, root;
+};
+*/
+
+
+
+
+//孩子表示法（指针方式）
+typedef struct node {
+	datatype data;
+	struct node *child[m];
+}node,*tree;
+//tree root;
+
+/*
+#define m 3
+typedef struct node {
+	datatype data;
+	node *child[m];
+}node,*tree;
+*/
+
+
+
+/*
+//孩子表示法（数组方式）
+#define m 3
+typedef struct node {
+	datatype data;
+	int child[m];
+}treenode;
+treenode tree[MAXLEN];
+int root;
+int length;
+
+
+#define m 3
+typedef struct node {
+	datatype data;
+	int child[m];
+}treenode;
+treenode tree[MAXLEN];
+int root;
+int length;
+*/
+
+
+/*
+//孩子表示法（链表方式）
+typedef struct chnode {
+	int child;
+	struct chnode *next;
+}chnode,*chpoint;
+typedef struct {
+	datatype data;
+	chpoint firstchild;
+}node;
+typedef struct {
+	node treelist[MAXLEN];
+	int length, root;
+}tree;
+
+
+
+typedef struct chnode {
+	int child;
+	struct chnode *next;
+}chnode,*chpoint;
+typedef struct {
+	datatype data;
+	chpoint firstchild;
+}node;
+typedef struct {
+	node treelist[MAXLEN];
+	int length, root;
+}tree;
+*/
+
+/*
+//孩子兄弟表示法
+typedef struct node {
+	datatype data;
+	struct node *firstchild, *rightsibling;
+}node,*pnode;
+pnode root;
+
+
+typedef struct node {
+	datatype data;
+	struct node *firstchild, *nextsibling;
+}node ,*pnode;
+pnode root;
+*/
+
+/*
+
+//前序遍历
+void preorder(tree p) {
+	if (p != NULL) {
+		printf("%c", p->data);
+		for (int i = 0;i < m;i++)
+			preorder(p->child[i]);
 	}
-	return ans;
 }
 
-
-int main() {
-	int n;
-	while (scanf("%d", &n) == 1 & n > 0) {
-		int *arr = new int[n + 1];
-		int i = 1;
-		while (i<n+1) {
-			cin >> arr[i++];
-		}
-		cout << LIS(arr, n) << endl;
+//后序遍历
+void postorder(tree p) {
+	if (p != NULL) {
+		for (int i = 0;i < m;i++)
+			postorder(p->child[i]);
+		printf("%c", p->data);
 	}
-	return 0;
+}
+
+//前序遍历建立树
+tree createtree() {
+	char ch;
+	tree t;
+	if ((ch == getchar()) == '#')
+		t = NULL;
+	else {
+		t = (tree)malloc(sizeof(node));
+		t->data = ch;
+		for (int i = 0;i < m;i++)
+			t->child[i] = createtree();
+	}
+	return t;
+}
+
+//树的层次遍历
+void levelorder(tree t) {
+	tree queue[100];
+	int f = 0, r = 1;  //f——队头 、 r——队尾
+	tree p;
+	queue[0] = t;
+	while (f < r) {
+		p = queue[f];
+		f++;
+		printf("%c", p->data);
+		for(int i=0;i<m;i++)  //将刚被访问的元素的所有子女结点依次进队
+			if (p->child[i]) {
+				queue[r] = p->child[i];
+				r++;
+			}
+	}
+}
+
+*/
+
+
+//将树的括号表示转换成树的孩子表示（数组方式）
+#define m 3
+#define MAXSIZE 20 //孩子表示法对应的数组大小
+#define BMAXSIZE 50  //括号表示对应的数组大小
+typedef struct node {
+	datatype data;
+	int child[m];
+}treenode;
+treenode tree[MAXSIZE];
+int root ,length;
+char p[BMAXSIZE];
+void bracktotree(char p[], int *root, int *length, treenode tree[]) {
+	int stack[MAXSIZE], top = 0;
+	int j = 0, k = 0, l, done = 1, i;
+	*root = 0;
+	tree[j].data = p[k];
+	k++;
+	for (int i = 0;i < m;i++)
+		tree[j].child[i] = -1;
+	while (done) {
+		if (p[k] == '(') {    //
+			stack[top] = j;
+			top++;
+			k++;
+		}
+		else if (p[k] == ')') {  //
+			top--;
+			if (top == 0)
+				done = 0;
+			else
+				k++;
+		}
+		else if (p[k] == ',') {   //
+			k++;
+		}
+		else {   //
+			j++;
+			tree[j].data = p[k];
+			for (i = 0;i < m;i++)
+				tree[j].child[i] = -1;
+			l = stack[top - 1];
+			i = 0;
+			//
+			while (tree[l].child[i] != -1)
+				i++;
+			tree[l].child[i] = j;
+			k++;
+		}
+	}
+	*length = j + 1;
 }
